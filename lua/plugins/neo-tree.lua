fa43758bea4061b38a797f678a3e0ccde16f6d0a
Jacob Patton <pattonjacob32@gmail.com>
2025-09-18 11:14:43 -0600

    init

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+	'nvim-neo-tree/neo-tree.nvim',
+	version = '*',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+		'MunifTanjim/nui.nvim',
+	},
+	lazy = false,
+	keys = {
+		{ '<C-n>', ':Neotree toggle reveal<CR>', desc = 'NeoTree reveal', silent = true },
+		{ '<leader>o', ':Neotree reveal<CR>', desc='NeoTree toggle', { silent = true } }
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					['\\'] = 'close_window',
+				},
+			},
+		},
+	},
+}
