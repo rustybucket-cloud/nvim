036b0e196cfe44c382dce48f50d95be8944d509a
Jacob Patton <pattonjacob32@gmail.com>
2025-09-24 11:28:52 -0600

    git keymaps

--- init.lua
+++ init.lua
@@ -262,6 +262,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>g', group = '[G]it', mode = { 'n', 'v' } },
       },
     },
   },
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,5 +3,8 @@ return {
   event = 'VeryLazy',
   config = function()
     vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit!<CR>', { desc = 'Git: Vertical Diff Split' })
+    vim.keymap.set('n', '<leader>gb', ':Git blame!<CR>', { desc = 'Git: Vertical Diff Split' })
+    vim.keymap.set('n', '<leader>gv', ':Git!<CR>', { desc = 'Git: Vertical Diff Split' })
+    vim.keymap.set('n', '<leader>gs', ':Git status<CR>', { desc = 'Git: Vertical Diff Split' })
   end,
 }
