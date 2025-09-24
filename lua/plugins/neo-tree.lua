976d45c1cd905d2896dbeb9642520d9904e2b226
Jacob Patton <pattonjacob32@gmail.com>
2025-09-24 09:49:33 -0600

    opencode

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,25 +2,25 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-	'nvim-neo-tree/neo-tree.nvim',
-	version = '*',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-		'MunifTanjim/nui.nvim',
-	},
-	lazy = false,
-	keys = {
-		{ '<C-n>', ':Neotree toggle reveal<CR>', desc = 'NeoTree reveal', silent = true },
-		{ '<leader>o', ':Neotree reveal<CR>', desc='NeoTree toggle', { silent = true } }
-	},
-	opts = {
-		filesystem = {
-			window = {
-				mappings = {
-					['\\'] = 'close_window',
-				},
-			},
-		},
-	},
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '<C-n>', ':Neotree toggle reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>n', ':Neotree reveal<CR>', desc = 'NeoTree toggle', { silent = true } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
 }
