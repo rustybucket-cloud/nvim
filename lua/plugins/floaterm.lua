6c70548292520dd968d2123e844d71d3b462a663
Jacob Patton <pattonjacob32@gmail.com>
2025-09-25 11:50:24 -0600

    floaterm

---
+++ lua/plugins/floaterm.lua
@@ -0,0 +1,51 @@
+local floatterm_open = 0
+
+local function floaterm_open()
+  floatterm_open = 1
+  vim.cmd 'FloatermToggle'
+end
+
+local function floaterm_close()
+  if floatterm_open == 1 then
+    vim.cmd 'FloatermToggle'
+    floatterm_open = 0
+  end
+end
+
+local function floaterm_new()
+  if floatterm_open == 1 then
+    vim.cmd 'FloatermNew'
+  end
+end
+
+local function floaterm_kill()
+  if floatterm_open == 1 then
+    vim.cmd 'FloatermKill'
+    floatterm_open = 0
+  end
+end
+
+local function floaterm_next()
+  if floatterm_open == 1 then
+    vim.cmd 'FloatermNext'
+  end
+end
+
+local function floaterm_prev()
+  if floatterm_open == 1 then
+    vim.cmd 'FloatermPrev'
+  end
+end
+
+return {
+  'voldikss/vim-floaterm',
+  config = function()
+    vim.keymap.set('n', "<C-'>", floaterm_open, { desc = 'Toggle Floaterm' })
+
+    vim.keymap.set('t', "<C-'>", floaterm_close, { desc = 'Toggle Floaterm' })
+    vim.keymap.set('t', '<C-n>', floaterm_new, { desc = 'New Floaterm' })
+    vim.keymap.set('t', '<C-q>', floaterm_kill, { desc = 'Kill Floaterm' })
+    vim.keymap.set('t', '<C-l>', floaterm_next, { desc = 'Next Floaterm' })
+    vim.keymap.set('t', '<C-h>', floaterm_prev, { desc = 'Previous Floaterm' })
+  end,
+}
